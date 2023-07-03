9eb9f636fdb765b0ea0a711bd263d7046db85ad6
Abdumalik <abdumaliktemirov>
2023-07-03 15:57:47 +0500

    desctop full finish

---
+++ js/main.js
@@ -0,0 +1,4 @@
+window.replainSettings = { id: '8a2ea20e-71f9-485a-9870-fac2666ac033' };
+(function(u){var s=document.createElement('script');s.async=true;s.src=u;
+var x=document.getElementsByTagName('script')[0];x.parentNode.insertBefore(s,x);
+})('https://widget.replain.cc/dist/client.js');
